44a9e36017ef7b6fc8b8f97392bba087620b55a1
Martins Aloba <aloba.martins@gmail.com>
2018-09-03 14:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // write your code below!
-var name = Matthew;
-var holiday = Christmas;
-var days = 25
+var name = "Matthew";
+var holiday = "Christmas";
+var days = 25;
+
 function happyHolidays() {
   return "Happy holidays!"
 }
 
 function happyHolidaysTo(name) {
-  
+  return `Happy holidays, ${name}!`
 }
