8d7872508d89a55182427f9245cb1ef6d0d0ddd3
Martins Aloba <aloba.martins@gmail.com>
2018-09-03 14:07:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // write your code below!
+var name = Matthew;
+var holiday = Christmas;
+var days = 25
 function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidays() {
-  return "Happy holidays!"
+function happyHolidaysTo(name) {
+  
 }
